20f7626efcabf73655ba3ffa2086f4b3a4b5802e
@Nepcoder098 <140476251+anony096@users.noreply.github.com>
2024-04-06 08:16:49 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 from flask import Flask, request, jsonify
 import requests
-from bs4 import BeautifulSoup
 import re
 
 app = Flask(__name__)
@@ -15,6 +14,13 @@ user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"
 ]
 
+def extract_download_link(html_content):
+    m = re.search(r'href="((http|https)://download[^"]+)', html_content)
+    if m:
+        return m.group(1)
+    else:
+        return None
+
 def get_mediafire_download_info(url):
     try:
         headers = {
@@ -23,30 +29,21 @@ def get_mediafire_download_info(url):
             "Useganet": "true"
         }
         page = requests.get(url, headers=headers)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        download_link_element = soup.find('a', {'id': 'downloadButton'})
-        download_link = download_link_element['href']
-        file_size_text = download_link_element.get_text(strip=True)
-        file_size = re.search(r'(\d+(\.\d+)?)MB', file_size_text)
-        if file_size:
-            file_size_mb = float(file_size.group(1))
-        else:
-            file_size_mb = None
-        return download_link, file_size_mb
+        direct_download_url = extract_download_link(page.text)
+        if not direct_download_url and 'Content-Disposition' in page.headers:
+            direct_download_url = page.headers['Content-Disposition'].split('filename=')[1].strip('"')
+        return direct_download_url
     except Exception as e:
         print(f"Error: {str(e)}")
-        return None, None
+        return None
 
 @app.route('/download', methods=['GET'])
 def download():
     url = request.args.get('url')
     if url:
-        direct_download_url, file_size_mb = get_mediafire_download_info(url)
+        direct_download_url = get_mediafire_download_info(url)
         if direct_download_url:
-            response_data = {'direct_download_url': direct_download_url}
-            if file_size_mb is not None:
-                response_data['file_size_mb'] = file_size_mb
-            return jsonify(response_data)
+            return jsonify({'direct_download_url': direct_download_url})
         else:
             return jsonify({'error': 'Direct download URL not found.'}), 404
     else:

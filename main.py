51127cd7ca5f054dbbccc5a6b6685d7ab7ea4d05
@Nepcoder098 <140476251+anony096@users.noreply.github.com>
2024-04-06 06:23:25 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
@@ -5,9 +6,23 @@ import re
 
 app = Flask(__name__)
 
+# List of user-agents
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.43",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/86.0.4240.198",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"
+]
+
 def get_mediafire_download_link(url):
     try:
-        page = requests.get(url)
+        headers = {
+            "User-Agent": random.choice(user_agents),
+            "Referer": url,
+            "Useganet": "true"
+        }
+        page = requests.get(url, headers=headers)
         soup = BeautifulSoup(page.content, 'html.parser')
         download_link = soup.find('a', {'id': 'downloadButton'})['href']
         return download_link
@@ -29,3 +44,4 @@ def download():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
